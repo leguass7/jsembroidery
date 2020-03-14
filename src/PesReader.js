d67a0216eb4ede1047e5a506254ae230f941733c
avatarsolucoes <dev@avatarsolucoesdigitais.com.br>
2020-03-13 21:50:13 -0300

    feat: organize package

---
+++ src/PesReader.js
@@ -0,0 +1,33 @@
+'use strict'
+
+exports.read = (f, out, settings = null) => {
+  //
+}
+
+exports.read_pes_string = (f) => {
+  //
+}
+
+exports.read_pes_metadata = (f, out) => {
+  //
+}
+
+exports.read_pes_thread = (f, threadlist) => {
+  //
+}
+
+exports.read_pes_header_version_1 = (f, out) => {
+  //
+}
+
+exports.read_pes_header_version_4 = (f, out) => {
+  //
+}
+
+exports.read_pes_header_version_5 = (f, out, threadlist) => {
+  //
+}
+
+exports.read_pes_header_version_6 = (f, out, threadlist) => {
+  //
+}
